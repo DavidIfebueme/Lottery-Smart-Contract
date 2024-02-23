70f33b56bad78dbd5d68b2eeed25fb3e749bcabe
DavidIfebueme <davidifebueme@gmail.com>
2024-02-23 08:08:24 +0100

    Changes to lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -1,10 +1,34 @@
-//SPDX-License-Identifier: MIT
-//import
+// SPDX-License-Identifier: MIT
 
+/// @dev solitity version.
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Lottery {
-    //ticketPrice
-    //maxNumberOfTickets
-    //roundDuration
+/** @title Main Lottery Contract.
+ * @dev Randomness provided internally in this version
+ * subsequent versions will use external randomness (Chainlink)
+ */
+contract Lottery is ReentracyGuard, Ownable{
+    using SafeERC20 for IERC;
+
+    uint256 public maximumNumberOfTickets =
+    uint256  public ticketPrice = 2 * 10**18; // V2 would use chainlink pricefeeds for a more dynamic outlook.
+    uint256 public ticketCommission =
+
+
+    address public treasuryAddy = //where all smart contract profits would be sent for safekeeping
+
+    enum Status{
+        Open,
+        Closed,
+        Claimable
+    }
+
+    struct Ticket{
+        uint256 ticketPrice;
+        
+    }
+
+    struct LotteryRound{
+        Status;
+    }
 }
